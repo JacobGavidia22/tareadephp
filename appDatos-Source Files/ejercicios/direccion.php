1ddfa4634f8144ca2c301c6da65d3dbc3696e4ea
CíborgAscend <lizz_gonzalez@hotmail.com>
2015-03-20 20:22:19 -0600

    repositoriouno

---
+++ appDatos-Source Files/ejercicios/direccion.php
@@ -0,0 +1,18 @@
+<?php
+$comentario;
+$calle;
+$colonia;
+$numero;
+$municipio;
+$departamento;
+
+{
+print $comentario = 'Mi direccion es:';
+print $calla = 'Calle continua a escuela,';
+print $cononia= ' Penitente abajo,';
+print $numero= 'Casa 56,';
+print $municipio= 'zacatecoluca,';
+print $departamento= ' Departamento La Paz,';
+
+}
+?>
